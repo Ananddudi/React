33e5f98c46ed508d98502df418699f07e185ffc4
Anand dudi <83879799+Ananddudi@users.noreply.github.com>
2022-03-10 00:03:33 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import {Contextapiis} from "./contextapi";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Contextapiis>
+      <App />
+    </Contextapiis>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
