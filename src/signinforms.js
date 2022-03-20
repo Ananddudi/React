259bb51bbfd132fab61c7dfee5aceb97d4a46fd6
Anand dudi <83879799+Ananddudi@users.noreply.github.com>
2022-03-20 14:45:54 +0530

    Update signinforms.js

--- src/signinforms.js
+++ src/signinforms.js
@@ -22,12 +22,17 @@ export const SignIn = () =>{
   const handlesubmit = async (e) => {
     e.preventDefault();
     let submitting = await fetch("", {
-      method: "POST",
-      mode: "cors",
+      method: "POST", //*GET, POST, PUT, DELETE, etc.
+      mode: "cors", //no-cors, *cors, same-origin
+      //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      //credentials: 'same-origin', // include, *same-origin, omit
       headers: {
         "content-type": "application/json",
         accept: "*",
+	// 'Content-Type': 'application/x-www-form-urlencoded',
       },
+      //redirect: 'follow', // manual, *follow, error,
+      //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
       body: JSON.stringify({ data: "data" }),
     });
     let response = submitting.json();
